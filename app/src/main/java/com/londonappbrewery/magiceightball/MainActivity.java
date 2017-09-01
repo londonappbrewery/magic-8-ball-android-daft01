f054a8cc3aa8856905df673c8276891ed70ee4cc
Daniel Morales <danielmorales0000@yahoo.com>
2017-09-01 01:23:47 -0700

    Project completed

--- app/src/main/java/com/londonappbrewery/magiceightball/MainActivity.java
+++ app/src/main/java/com/londonappbrewery/magiceightball/MainActivity.java
@@ -1,7 +1,17 @@
 package com.londonappbrewery.magiceightball;
 
+import android.content.DialogInterface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.util.Random;
+
+import static android.R.attr.button;
+import static com.londonappbrewery.magiceightball.R.id.roll_button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +19,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Button roll_button = (Button) findViewById(R.id.roll_button);
+
+        final ImageView left_dice = (ImageView) findViewById(R.id.ball);
+
+        final int[] eball = {
+                R.drawable.ball1,
+                R.drawable.ball2,
+                R.drawable.ball3,
+                R.drawable.ball4,
+                R.drawable.ball5
+        };
+
+        roll_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Random rand = new Random();
+                int num = rand.nextInt(5);
+                left_dice.setImageResource(eball[num]);
+            }
+        });
+        };
     }
-}
